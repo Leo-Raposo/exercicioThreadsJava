880201610753d8ca29867a84ed72049572db2206
Leonardo Raposo <104743570+Leo-Raposo@users.noreply.github.com>
2024-07-16 20:21:22 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,9 @@ public class Main {
     public static void main(String[] args) {
 
         Download download1 = new Download("Black Pumas - Colors.mp3");
-        Download2 download2 = new Download2("Arctic Monkeys - R U Mine.mp3");
-        Download3 download3 = new Download3("Vidas ao Vento.mp4");
-        Download4 download4 = new Download4("Teddy Swims - Lose Control.mp3");
+        Download download2 = new Download("Arctic Monkeys - R U Mine.mp3");
+        Download download3 = new Download("Vidas ao Vento.mp4");
+        Download download4 = new Download("Teddy Swims - Lose Control.mp3");
 
         download1.start();
         download2.start();
